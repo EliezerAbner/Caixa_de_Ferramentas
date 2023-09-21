2e6587fb69ee02c43e7d8aa0a100da2ac82e08c7
EliezerAbner <eliezerabnerpo@gmail.com>
2023-09-21 15:48:22 -0300

    Alterações na View Verificação

--- Mobile/AppCaixaFerramentas/AppCaixaFerramentas/Views/PageHome.xaml.cs
+++ Mobile/AppCaixaFerramentas/AppCaixaFerramentas/Views/PageHome.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AppCaixaFerramentas.Controllers;
 using AppCaixaFerramentas.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
--- Mobile/AppCaixaFerramentas/AppCaixaFerramentas/Views/PageVericacao.xaml.cs
+++ Mobile/AppCaixaFerramentas/AppCaixaFerramentas/Views/PageVericacao.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,21 @@ namespace AppCaixaFerramentas.Views
         public PageVericacao()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            var duration = TimeSpan.FromMilliseconds(50);
+            Vibration.Vibrate(duration);
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                scanResultTest.Text = result.Text + " (type: " + result.BarcodeFormat.ToString() + ")";
+            });
         }
     }
 }
